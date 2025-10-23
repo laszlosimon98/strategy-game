b181c0e16466f8d8bf12c2f307f9cf36081d13d2
Laszlo Simon <98laszlosimon@gmail.com>
2025-10-23 11:06:00 +0200

    refactor: enhance image loader

--- server/src/utils/imageLoader.ts
+++ server/src/utils/imageLoader.ts
@@ -6,83 +6,46 @@ import { settings } from "@/settings";
 export class Loader {
   private constructor() {}
 
-  private static async getFiles(_dir: string): Promise<string[]> {
-    const result: string[] = [];
-    try {
-      const files: string[] = await fs.readdir(_dir, { recursive: true });
-      files.forEach((file) => {
-        if (file.split(".")[1] === "png") {
-          result.push(file.replace(/\\/g, "/"));
-        }
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  private static async getFiles(dir: string): Promise<string[]> {
+    const entries = await fs.readdir(dir, { withFileTypes: true });
+    const files = await Promise.all(
+      entries.map(async (entry) => {
+        const fullPath = path.join(dir, entry.name);
+        return entry.isDirectory() ? await this.getFiles(fullPath) : fullPath;
+      })
+    );
 
-    return result;
+    return files.flat().filter((file) => file.endsWith(".png"));
   }
 
-  public static async loadImages(_dir: string) {
-    const files: string[] = await this.getFiles(_dir);
-    const result = this.generateRoutes(_dir, files);
-    return result;
+  public static async loadImages(baseDir: string) {
+    const files = await this.getFiles(baseDir);
+    return this.generateRoutes(baseDir, files);
   }
 
-  private static generateRoutes(_dir: string, files: string[]) {
-    const routes: any = {};
-    const paths: string[] = [];
+  private static generateRoutes(baseDir: string, files: string[]) {
+    const routes: Record<string, any> = {};
 
-    files.forEach((file) => {
-      const imageDimensions = sizeOf(`${_dir}/${file}`);
-      const { width, height } = imageDimensions;
+    for (const filePath of files) {
+      const { width, height } = sizeOf(filePath);
+      const relativePath = path.relative(baseDir, filePath).replace(/\\/g, "/");
+      const parts = relativePath.split("/");
+      const name = path.basename(filePath, ".png");
+      const dirParts = parts.slice(0, -1);
 
-      const fileLength = file.split("/").length;
-      const name = file.split("/")[fileLength - 1].split(".")[0];
+      const url = `${settings.serverUrl}/assets/${relativePath}`;
 
-      const _path: string[] = file.split("/").splice(0, fileLength - 1);
+      let current = routes;
+      for (const dir of dirParts) {
+        if (!current[dir]) current[dir] = {};
+        current = current[dir];
+      }
 
-      const url: string = `${settings.serverUrl}/assets/${path
-        .join(..._path)
-        .replace(/\\/g, "/")}/${name}.png`;
-      const length = _path.length;
-
-      routes[_path[length - 1]] = {
-        ...routes[_path[length - 1]],
-        [name]: {
-          url,
-          dimensions: { width, height },
-        },
+      current[name] = {
+        url,
+        dimensions: { width, height },
       };
-
-      if (!paths.includes(_path.join("_"))) {
-        paths.push(_path.join("_"));
-      }
-    });
-
-    paths.forEach((_path) => {
-      const dirs = _path.split("_");
-
-      for (let i = dirs.length - 1; i > 0; --i) {
-        routes[dirs[i - 1]] = {
-          ...routes[dirs[i - 1]],
-          [dirs[i]]: routes[dirs[i]],
-        };
-      }
-    });
-
-    Object.keys(routes).forEach((key) => {
-      const arr: string[] = [
-        "buildings",
-        "units",
-        "ground",
-        "menu",
-        "ui",
-        "obstacles",
-      ];
-      if (!arr.some((dirName) => dirName === key)) {
-        delete routes[key];
-      }
-    });
+    }
 
     return routes;
   }
