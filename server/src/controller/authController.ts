80a90ce3a7b7aff043fae2161cb2d923c2e6deb2
Laszlo Simon <98laszlosimon@gmail.com>
2025-10-29 22:44:42 +0100

    fix: registration and login if no db

--- server/src/controller/authController.ts
+++ server/src/controller/authController.ts
@@ -12,26 +12,26 @@ interface DecodedToken {
 export const handleRegister = async (request: Request, response: Response) => {
   const { username, password } = request.body;
 
-  const user = await prismaService.user.findUnique({
-    where: {
-      username,
-    },
-  });
-
-  if (user) {
-    return response.status(400).send("A felhasználónév foglalt!");
-  }
-
-  if (!username || !password) {
-    return response.status(400).send("Az adatok megadása kötelező!");
-  }
-
-  const hashedPassword: string = await bcrypt.hash(
-    password,
-    parseInt(process.env.HASHROUND || "10")
-  );
-
   try {
+    const user = await prismaService.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    if (user) {
+      return response.status(400).send("A felhasználónév foglalt!");
+    }
+
+    if (!username || !password) {
+      return response.status(400).send("Az adatok megadása kötelező!");
+    }
+
+    const hashedPassword: string = await bcrypt.hash(
+      password,
+      parseInt(process.env.HASHROUND || "10")
+    );
+
     await prismaService.user.create({
       data: {
         username,
@@ -42,59 +42,63 @@ export const handleRegister = async (request: Request, response: Response) => {
 
     return response.status(200).send("Sikeres regisztráció!");
   } catch (err) {
-    return response.status(500).send("Váratlan hiba történt!");
+    return response.status(500).send("Sikertelen regisztráció!");
   }
 };
 
 export const handleLogin = async (request: Request, response: Response) => {
   const { username, password } = request.body;
 
-  const user = await prismaService.user.findUnique({
-    where: {
-      username,
-    },
-  });
+  try {
+    const user = await prismaService.user.findUnique({
+      where: {
+        username,
+      },
+    });
 
-  if (!user) {
+    if (!user) {
+      return response.status(400).send("A felhasználó nem található!");
+    }
+
+    const isPasswordValid: boolean = await bcrypt.compare(
+      password,
+      user.password
+    );
+
+    if (!isPasswordValid) {
+      return response.status(400).send("Rossz felhasználónév vagy jelszó!");
+    }
+
+    const accessToken = jwt.sign(
+      { username: user.username },
+      process.env.JWT_ACCESS_TOKEN_SECRET || "default_access_secret",
+      { expiresIn: "5m" }
+    );
+
+    const refreshToken = jwt.sign(
+      { username: user.username },
+      process.env.JWT_REFRESH_TOKEN_SECRET || "default_refresh_secret",
+      { expiresIn: "1d" }
+    );
+
+    await prismaService.user.update({
+      where: { username },
+      data: {
+        refreshToken,
+      },
+    });
+
+    response.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    });
+
+    return response.status(200).json({ accessToken });
+  } catch (e) {
     return response.status(400).send("A felhasználó nem található!");
   }
-
-  const isPasswordValid: boolean = await bcrypt.compare(
-    password,
-    user.password
-  );
-
-  if (!isPasswordValid) {
-    return response.status(400).send("Rossz felhasználónév vagy jelszó!");
-  }
-
-  const accessToken = jwt.sign(
-    { username: user.username },
-    process.env.JWT_ACCESS_TOKEN_SECRET || "default_access_secret",
-    { expiresIn: "5m" }
-  );
-
-  const refreshToken = jwt.sign(
-    { username: user.username },
-    process.env.JWT_REFRESH_TOKEN_SECRET || "default_refresh_secret",
-    { expiresIn: "1d" }
-  );
-
-  await prismaService.user.update({
-    where: { username },
-    data: {
-      refreshToken,
-    },
-  });
-
-  response.cookie("refreshToken", refreshToken, {
-    httpOnly: true,
-    sameSite: "none",
-    secure: true,
-    maxAge: 24 * 60 * 60 * 1000, // 1 day
-  });
-
-  return response.status(200).json({ accessToken });
 };
 
 export const handleRefreshToken = async (
